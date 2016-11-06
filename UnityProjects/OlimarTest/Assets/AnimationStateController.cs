402e45b4aa46eea2fdaa92d9d25464ffd6dd4f52
Will Anderson <andersow@usc.edu>
2016-11-06 01:19:50 -0700

    Playing with different ways of importing from maya.

---
+++ UnityProjects/OlimarTest/Assets/AnimationStateController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum AnimationState
+{
+    Idle,
+    Walking,
+    Attacking
+};
+
+public class AnimationStateController : MonoBehaviour {
+    public AnimationState currentAnimationState;
+    public Animator mAnimator;
+	
+	void Start () {
+        currentAnimationState = AnimationState.Idle;
+        mAnimator = GetComponent<Animator>();
+        AnimationState temp = AnimationState.Idle;
+        UpdateAnimationState(temp);
+	}
+
+    void UpdateAnimationState(AnimationState newAnimationState)
+    {
+        currentAnimationState = newAnimationState;
+        if (mAnimator != null)
+        {
+            switch (currentAnimationState)
+            {
+                case AnimationState.Idle:
+                    mAnimator.SetBool("IsWalking", false);
+                    mAnimator.SetBool("IsIdle", true);
+                    mAnimator.SetBool("IsAttacking", false);
+                    break;
+                case AnimationState.Walking:
+                    mAnimator.SetBool("IsWalking", true);
+                    mAnimator.SetBool("IsIdle", false);
+                    mAnimator.SetBool("IsAttacking", false);
+                    break;
+                case AnimationState.Attacking:
+                    mAnimator.SetBool("IsWalking", false);
+                    mAnimator.SetBool("IsIdle", false);
+                    mAnimator.SetBool("IsAttacking", true);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
